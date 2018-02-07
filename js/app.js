9ad91dc8463be798c5f6f0760c9fa54a2e74bc22
delacruzPamela <delacruzlozano.p@gmail.com>
2018-02-07 17:14:25 -0600

    Añadiendo funcionabilidad para Api maps y autocompletado de los inputs

--- js/app.js
+++ js/app.js
@@ -1,11 +1,25 @@
-/* VERSIÓN 1 */
-$(document).ready(function() {
+// Mapa
+function initMap() {
+  let coordinate = {lat: -12.1191427,
+    lng: -77.0349046};
+  let map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 15,
+    center: coordinate
+  });
+  
+  // Muestra la posicion segun las coordenadas mencionadas
+  let marker = new google.maps.Marker({
+    position: coordinate,
+    map: map
+  });
 
-});
+  // Autocompletado
+  let inputOrigin = document.getElementById('origin');
+  let inputDestination = document.getElementById('destination');
 
+  new google.maps.places.Autocomplete(inputOrigin);
+  new google.maps.places.Autocomplete(inputDestination);
 
-/* VERSIÓN 2 */
-function begin() {
-
+  // Trazar ruta
+  /* Aqui va codigo-------- */
 }
-$(document).ready(begin);
